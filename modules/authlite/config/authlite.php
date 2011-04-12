46d4ced6885d060ee5583bf1fc4793f5cc8b89e5
ekala <ekala@192.168.1.141>
2011-04-12 08:57:00 -0700

    * Added authlite module - for dashboard logins

---
+++ modules/authlite/config/authlite.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * User model
+ */
+$config['user_model'] = 'dashboard_user';
+
+/**
+ * Username column
+ */
+$config['username'] = 'username';
+
+// Email column - To allow users to login in via email
+$config['email'] = 'email';
+
+/**
+ * Password column
+ */
+$config['password'] = 'password';
+
+/**
+ * Session column
+ */
+$config['session'] = 'session_key';
+
+/**
+ * Type of hash to use for passwords. Any algorithm supported by the hash function
+ * can be used here.
+ * @see http://php.net/hash
+ * @see http://php.net/hash_algos
+ */
+$config['hash_method'] = 'sha1';
+
+/**
+ * Set the auto-login (remember me) cookie lifetime, in seconds. The default
+ * lifetime is two weeks.
+ */
+$config['lifetime'] = 1209600;
+
+/**
+ * Set the session key that will be used to store the current user.
+ */
+$config['session_key'] = 'huduma_dashboard_user';
